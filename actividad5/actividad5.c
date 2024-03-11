3caf433907ca51a31b3f26bbade7615478cd7671
Cristian Pereira <3013484210101@ingenieria.usac.edu.gt>
2024-03-10 21:29:36 -0600

    actividad5

--- actividad5/actividad5.c
+++ actividad5/actividad5.c
@@ -11,6 +11,8 @@ void *thread_function(void *arg) {
 int main() {
     pid_t pid;
     pthread_t tid;
+    pthread_attr_t attr;
+
     printf("Proceso Principal\n");
     printf("Proceso Creado 1\n");
     pid = fork(); //FORK1: Se crea un proceso hijo (Hijo 1)
@@ -19,7 +21,9 @@ int main() {
         printf("Proceso Creado 2\n");
         fork(); // FORK2: Se crea otro proceso hijo (Hijo 2)
         printf("Hilo Creado\n");
-        pthread_create(&tid, NULL, thread_function, NULL);
+        pthread_attr_init(&attr);
+        pthread_create(&tid, &attr, thread_function, NULL);
+        pthread_join(tid, NULL);
     }
 
     printf("Proceso Creado 3\n");
